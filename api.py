1f50cba9c8bd4ea3c639a0b6f506e33d4de7701b
Creeper512 <creeper938@outlook.com>
2023-05-20 21:03:56 +0800

    update

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ def load_image_links(link_file):
         return [line.strip() for line in file]
 
 # 随机重定向到一个图片链接
-@app.route('/')
+@app.route('/api.py')
 def redirect_to_random_image():
     image_links = load_image_links('image_url.txt')  # 替换为你的图片链接文件路径
     random_link = random.choice(image_links)
