060866173d9ddd67bcacb9ab6d335e73cc6dd9d3
Creeper512 <creeper938@outlook.com>
2023-05-21 12:30:28 +0800

    fix

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 // 读取图片链接
-$urls = file('../image_url.txt', FILE_IGNORE_NEW_LINES);
+$urls = file('image_url.txt', FILE_IGNORE_NEW_LINES);
 
 // 随机选择一个链接
 $randomUrl = $urls[array_rand($urls)];
