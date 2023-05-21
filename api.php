ea32c38d3ba4114288c1d4f67240c42bc048f086
Creeper512 <creeper938@outlook.com>
2023-05-21 12:16:16 +0800

    使用 php 实现

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+// 读取图片链接
+$urls = file('image_url.txt', FILE_IGNORE_NEW_LINES);
+
+// 随机选择一个链接
+$randomUrl = $urls[array_rand($urls)];
+
+// 执行重定向
+header('Location: ' . $randomUrl);
+exit;
+?>
