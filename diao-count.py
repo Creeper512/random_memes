babde3e2539a7366bb94e15fef028db57c2ea9d6
Creeper512 <creeper938@outlook.com>
2023-05-21 09:10:31 +0800

    更新

--- diao-count.py
+++ diao-count.py
@@ -1,18 +1,30 @@
-import json
-from flask import Flask, redirect
+import requests
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# 读取image_url.txt文件中的行数
-def count_lines(file):
-    with open(file, 'r') as f:
-        return sum(1 for _ in f)
+def get_last_commit_time(repo_owner, repo_name, file_path):
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits?path={file_path}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        commits = response.json()
+        if commits:
+            last_commit_time = commits[0]['commit']['committer']['date']
+            return last_commit_time
+    return None
 
-@app.route('/diao-count.py')
+@app.route('/diao-count')
 def get_diao_count():
-    line_count = count_lines('image_url.txt')  # 替换为你的图片链接文件路径
-    data = {'count': line_count}
-    return json.dumps(data)
+    repo_owner = "Creeper512"
+    repo_name = "random_memes"
+    file_path = "image_url.txt"
+    last_commit_time = get_last_commit_time(repo_owner, repo_name, file_path)
+    count = sum(1 for line in open(file_path)) if last_commit_time else 0
+    data = {
+        'count': count,
+        'last_commit_time': last_commit_time
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
