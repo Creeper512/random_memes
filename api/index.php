0fbf725e0bbe2492dd69d48f3a1da45612a529be
Creeper512 <creeper938@outlook.com>
2023-05-21 13:33:34 +0800

    更好的请求

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 // 获取上级目录路径
 $parentDirectory = dirname(__DIR__);
 
