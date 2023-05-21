e879acdd746fa8617486cdc1d66b32b062c23145
Creeper512 <creeper938@outlook.com>
2023-05-21 12:00:27 +0800

    修复重复命中一张图片的问题

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, redirect, current_app
+from flask import Flask, redirect, g
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,10 +13,10 @@ def load_image_links(link_file):
 # 随机重定向到一个图片链接
 @app.route('/api.py')
 def redirect_to_random_image():
-    image_links = current_app.image_links
+    image_links = g.image_links
     random_link = random.choice(image_links)
     return redirect(random_link)
 
 if __name__ == '__main__':
-    app.image_links = load_image_links('image_url.txt')  # 在应用程序上下文之外加载图片链接
+    app.before_request(lambda: setattr(g, 'image_links', load_image_links('image_url.txt')))
     app.run()
